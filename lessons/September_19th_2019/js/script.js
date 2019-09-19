9f95e8f49c4e1b27568e8cd157e0cb7217e2fcb4
Sylvain Tran <sylvain.serey.tran@protonmail.com>
2019-09-19 14:12:54 -0400

    Class 3: Continuing Examples, Correcting a few things on the exercise 1
    Class 3: Continuing Examples, Correcting a few things on the exercise 1.

--- lessons/September_19th_2019/js/script.js
+++ lessons/September_19th_2019/js/script.js
@@ -1,35 +1,45 @@
 /*****************
 
-Title of Project
-Author Name
-
-This is a template. You must fill in the title,
-author, and this description to match your project!
+Class 3: 19 September 2019
+Sylvain Tran
 
 ******************/
 
-// preload()
-//
-// Description of preload
-
-function preload() {
-
-}
-
-
-// setup()
-//
-// Description of setup
+let x = 0;
+let y = 0;
+let vx = 2;
+let vy = -2;
+let speed = 0.005;
+let ax = 0.25;
+let ay = 0.25;
 
 function setup() {
-
+  createCanvas(500, 500);
+  x = width / 2;
+  y = height / 2;
 }
 
-
-// draw()
-//
-// Description of draw()
-
 function draw() {
+  vx += ax;
+  vy += ay;
 
+  x += vx;
+  y += vy;
+
+  ellipse(x += vx * speed, y += vy, 50, 50);
+
+  if( x > width || x > height ){
+    x = width / 2;
+    fill(255);
+    rect( (width / 2), height / 2, 50, 50);
+  }
+
+  if(mouseX > width / 2)
+  {
+    background(0);
+  }
+  else
+  {
+    background(255);
+  }
 }
