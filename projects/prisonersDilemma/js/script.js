8c4d00583568d760ba96c5932325e1e7a5b8426c
Sylvain Tran <45033804+peelingSun@users.noreply.github.com>
2019-09-22 19:38:51 -0400

    SideProject1: Added empty script
    SideProject1: Added empty script for prisoner's dilemma.

---
+++ projects/prisonersDilemma/js/script.js
@@ -0,0 +1,38 @@
+/**
+   
+    Title: Prisoner's Dilemma
+    Author: Sylvain Tran
+
+    Goal of program:
+    Experimental game based on the prisoner's 
+    dilemma in decision-making psychology.
+
+    To practice OOP as well.
+*/
+
+/**
+
+ 
+ 
+*/
+function preload() {
+
+}
+
+/**
+    
+ 
+ 
+*/
+function setup() {
+
+}
+
+/**
+    
+ 
+ 
+*/
+function draw() {
+
+}
