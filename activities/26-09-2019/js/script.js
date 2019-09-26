8722f18476151b69c5b885b847614bc04b0f57a7
Sylvain Tran <sylvain.serey.tran@protonmail.com>
2019-09-26 13:54:23 -0400

    Ex3: Added class activity files and base files for exercise 3.
    Ex3: Added class activity files and base files for exercise 3.

---
+++ activities/26-09-2019/js/script.js
@@ -0,0 +1,21 @@
+"use strict";
+
+let startX;
+let startY;
+let segmentRadius = 20;
+function setup() {
+  createCanvas(640,480);
+  noStroke();
+  fill(80,200,80);
+  startX = width/5;
+  startY = height/2;
+}
+function draw() {
+  background(200,250,200);
+  ellipse(startX,startY,segmentRadius*2);
+  ellipse(startX + segmentRadius*1.5,startY,segmentRadius*2);
+  ellipse(startX + segmentRadius*3,startY,segmentRadius*2);
+  ellipse(startX + segmentRadius*4.5,startY,segmentRadius*2);
+  ellipse(startX + segmentRadius*6,startY,segmentRadius*2);
+  // aaaaaand so on...
+}
