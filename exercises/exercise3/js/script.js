0983e95afc744f434257c8a3db1c20fc43f6b0c8
SylvainTran <sylvain.serey.tran@protonmail.com>
2019-10-03 21:57:59 -0400

    Ex3: trimmed down the game... fixed the victory animation.

--- exercises/exercise3/js/script.js
+++ exercises/exercise3/js/script.js
@@ -29,6 +29,9 @@ let targetY;
 let targetImage;
 let targetImageSizeX;
 let targetImageSizeY;
+let targetImageSpeed = 2;
+let vx = 1;
+let vy = 1;
 
 // The ten decoy images
 let decoyImage1;
@@ -45,16 +48,12 @@ let decoyImage10;
 // The number of decoys to show on the screen, randomly
 // chosen from the decoy images
 let numDecoys = 100;
-
 // Keep track of whether they've won
 let gameOver = false;
-
 // Or lost
 let lost = false;
-
 // Amount of times the player has won without losing (without having had clicked too many times)
 let streakWins = 0;
-
 // How much the decoy image sizes increment each time a game is won
 let sizeIncrement = 50;
 
@@ -157,192 +156,204 @@ function setupDecoys(decoySize = targetImageSizeX) {
 
 */
 function draw() {
-  checkIfGameOver();
-  // Register for inputs (Press the Enter key to restart the game)
-  handleInputs();
   // Draw the canvas
   drawGUI();
-  handleClicks();
-}
+  handleInputs();
 
-function drawGUI() {
-  let guiXPos = width / 1.2;
-  let guiYPos = 0;
-
-  // Draw a background rectangle at the top right of the canvas
-  push();
-  noStroke();
-  fill(45, 255, 100);
-  rect(guiXPos, guiYPos, width / 6, height / 4);
-  image(targetImage, guiXPos += width / 12, guiYPos += height / 10, width / 12, targetImageSizeY);
-  fill(255);
-  textSize(32);
-
-  // Center the text below the picture of the target, centered
-  text("Find me!", guiXPos -= width / 24, guiYPos += height / 8);
-  pop();
-
-  // Display streak wins yet (a template litteral woo!)
-  push();
-  fill(120, 0, 120);
-  textSize(40);
-  text(`Streak Wins: ${streakWins}`, guiXPos - 400, guiYPos - 200);
-  pop();
-
-  // Display number of clicks so far and how many left before losing
-  push();
-  fill(120, 0, 120);
-  textSize(40);
-  text(`Times Clicked: ${numbersOfClicks}`, guiXPos - 800, guiYPos - 200);
-  pop();
-
-  // Display number of clicks so far and how many left before losing: Not sure how to keep
-  // the random bg color while displaying the text below correctly.
-  push();
-  fill(120, 0, 120);
-  textSize(40);
-  text(`Max clicks left: ${maxClicks - numbersOfClicks}`, guiXPos - 1200, guiYPos - 200);
-  pop();
-}
-
-function handleClicks() {
-  if (mouseIsPressed) {
-    numbersOfClicks++;
-  }
-  console.log("Number of clicks: " + numbersOfClicks);
-}
-
-/**
-  Displays win text if the player clicked the target image.
-
-*/
-function displayWinText() {
-  textFont("Helvetica");
-  textSize(128);
-  noStroke();
-  fill(255);
-  text("YOU WINNED!", width / 4, height / 2);
-  displayCircleAroundTarget();
-}
-
-/**
-  Checks if the game is over because the player clicked too many times, or if they actually found
-  the target.
-
-*/
-function checkIfGameOver() {
-  if (numbersOfClicks >= maxClicks) {
-    lost = true;
-  }
   if (gameOver) {
-    gameOver = false;
-    streakWins++;
-    displayWinText();
-    animateTargetUponWin();
-  }
-  if (lost) {
-    streakWins = 0;
-    background(255);
-    textSize(100);
-    fill(255, 0, 0);
-    text("Get good", width / 3, height / 2);
-    text("Press Enter to restart...", width / 3, height / 1.5);
-    lost = false;
-  }
-}
+    // Prepare our typography
+    textFont("Helvetica");
+    textSize(128);
+    noStroke();
+    fill(random(255));
 
-function displayCircleAroundTarget() {
-  push();
-  noFill();
-  stroke(random(255));
-  strokeWeight(10);
-  ellipse(targetX, targetY, targetImage.width, targetImage.height);
-  pop();
-}
+    // Tell them they won!
+    text("YOU WINNED!",width/2,height/2);
+    background(0);
+    displayCircleAroundTarget();
 
-function increaseDifficulty() {
-    numbersOfClicks = 0; // Reset the number of clicks to avoid losing
-    // Random new background color
-    background(random(0, 255), random(0, 255), random(0, 255));
-    setupDecoys(targetImageSizeX += 50); // Level difficulty scaling up
-}
-/**
-  Moves the target randomly upon having won.
+    // victory animation
+    vx += targetImageSpeed;
+    vy += targetImageSpeed;
 
-*/
-function animateTargetUponWin() {
-  // Inner canvas settings
-  let innerCanvasWidth = width - innerMargins;
-  let innerCanvasHeight = height - innerMargins;
-  let innerCanvasHeightTop = innerMargins;
-
-  // Velocity parameters
-  let automaticVx = 1;
-  let automaticVy = 1;
-
-  // Number of times we animate the target image after victory
-  let animationCounter = 5000;
-  let timesAnimated = 0;
-  for(; timesAnimated <= 5000; timesAnimated++){
-    automaticVx = random(0, 50);
-    automaticVy = random(0, 50);
-
-    targetX += automaticVx;
-    targetY += automaticVy;
-
-    // Make the dog appear at the other end of the canvas as if it rained
-    if (targetX < 0 || targetX >= innerCanvasWidth) {
-      targetX = -automaticVx;
-    } else if (targetY < 0 || targetY >= innerCanvasWidth) {
-      targetY = -automaticVy;
+    if (targetX < 0 || targetX > width) {
+      vx = -vx;
+    }
+    if (targetY < 0 || targetY > height) {
+      vy = -vy;
     }
 
-    // The actual display
-    background(0); // redraw the background in black
-    //textSize(100);
-    //fill(255, 0, 0);
-    //text("It's raining dogs!", width / 4, height / 2);
+    targetX += vx;
+    targetY += vy;
+
     image(targetImage, targetX, targetY, targetImageSizeX, targetImageSizeY);
     displayWinText();
   }
 }
 
-/**
-  Resets the statistics (number of clicks).
+  function drawGUI() {
+    let guiXPos = width / 1.2;
+    let guiYPos = 0;
 
-*/
-function resetGame() {
-  numbersOfClicks = 0;
-  gameOver = false;
-  clear();
-  setupDecoys(targetImageSizeX);
-  drawGUI();
-}
+    // Draw a background rectangle at the top right of the canvas
+    push();
+    noStroke();
+    fill(45, 255, 100);
+    rect(guiXPos, guiYPos, width / 6, height / 4);
+    image(targetImage, guiXPos += width / 12, guiYPos += height / 10, width / 12, targetImageSizeY);
+    fill(255);
+    textSize(32);
 
-/**
-  Checks if the player clicked on the target and if so tells them they won
+    // Center the text below the picture of the target, centered
+    text("Find me!", guiXPos -= width / 24, guiYPos += height / 8);
+    pop();
 
-*/
-function mousePressed() {
-  if (mouseX > targetX - targetImage.width / 2 && mouseX < targetX + targetImage.width / 2) {
-    if (mouseY > targetY - targetImage.height / 2 && mouseY < targetY + targetImage.height / 2) {
-      checkIfGameOver();
-      gameOver = true;
-      console.log("Target pressed");
+    // Display streak wins yet (a template litteral woo!)
+    push();
+    fill(120, 0, 120);
+    textSize(40);
+    text(`Streak Wins: ${streakWins}`, guiXPos - 400, guiYPos - 200);
+    pop();
+
+    // Display number of clicks so far and how many left before losing
+    push();
+    fill(120, 0, 120);
+    textSize(40);
+    text(`Times Clicked: ${numbersOfClicks}`, guiXPos - 800, guiYPos - 200);
+    pop();
+
+    // Display number of clicks so far and how many left before losing: Not sure how to keep
+    // the random bg color while displaying the text below correctly.
+    push();
+    fill(120, 0, 120);
+    textSize(40);
+    text(`Max clicks left: ${maxClicks - numbersOfClicks}`, guiXPos - 1200, guiYPos - 200);
+    pop();
+  }
+
+  function handleClicks() {
+    if (mouseIsPressed) {
+      numbersOfClicks++;
+    }
+    console.log("Number of clicks: " + numbersOfClicks);
+  }
+
+  /**
+    Displays win text if the player clicked the target image.
+
+  */
+  function displayWinText() {
+    textFont("Helvetica");
+    textSize(128);
+    noStroke();
+    fill(255);
+    text("YOU WINNED!", width / 4, height / 2);
+    displayCircleAroundTarget();
+  }
+
+  /**
+    Checks if the game is over because the player clicked too many times, or if they actually found
+    the target.
+
+  */
+  function checkIfGameOver() {
+    if (numbersOfClicks >= maxClicks) {
+      lost = true;
+    }
+    if (gameOver) {
+      //gameOver = false;
+      streakWins++;
+      displayWinText();
+      //animateTargetUponWin();
+    }
+    if (lost) {
+      streakWins = 0;
+      background(255);
+      textSize(100);
+      fill(255, 0, 0);
+      text("Get good", width / 3, height / 2);
+      text("Press Enter to restart...", width / 3, height / 1.5);
+      lost = false;
     }
   }
-}
 
-/**
-  Handle inputs for resetting the game and increasing difficulty buttons.
+  function displayCircleAroundTarget() {
+    push();
+    noFill();
+    stroke(random(255));
+    strokeWeight(10);
+    ellipse(targetX, targetY, targetImage.width, targetImage.height);
+    pop();
+  }
 
-*/
-function handleInputs() {
-  if(keyIsDown(ENTER)) {
-    console.log("Enter pressed... resetting game.");
+  function increaseDifficulty() {
+    numbersOfClicks = 0; // Reset the number of clicks to avoid losing
     resetGame();
+    // Random new background color
+    //background(random(0, 255), random(0, 255), random(0, 255));
+    setupDecoys(targetImageSizeX += sizeIncrement); // Level difficulty scaling up
   }
-  else if (keyIsDown(SHIFT)) {
-    increaseDifficulty();
+  /**
+    Moves the target randomly upon having won.
+
+  */
+  function animateTargetUponWin() {
+    console.log("how many times am i called?");
+    // Inner canvas settings
+    let innerCanvasWidth = width - innerMargins;
+    let innerCanvasHeight = height - innerMargins;
+    let innerCanvasHeightTop = innerMargins;
+
+    // Velocity parameters
+    let automaticVx = 1;
+    let automaticVy = 1;
+
+    // Number of times we animate the target image after victory
+    let animationCounter = 5000;
+    let timesAnimated = 0;
+    for (; timesAnimated < 5000; timesAnimated++) {
+      console.log("how many times am i called? 2");
+
+      displayWinText();
+    }
   }
-}
+
+  /**
+    Resets the statistics (number of clicks).
+
+  */
+  function resetGame() {
+    numbersOfClicks = 0;
+    gameOver = false;
+    clear();
+    setupDecoys(targetImageSizeX);
+    drawGUI();
+  }
+
+  /**
+    Checks if the player clicked on the target and if so tells them they won
+
+  */
+  function mousePressed() {
+    if (mouseX > targetX - targetImage.width/2 && mouseX < targetX + targetImage.width/2) {
+      // Check if the cursor is also in the y range of the target
+      // i.e. check if it's within the top and bottom of the image
+      if (mouseY > targetY - targetImage.height/2 && mouseY < targetY + targetImage.height/2) {
+        gameOver = true;
+      }
+    }
+  }
+
+  /**
+    Handle inputs for resetting the game and increasing difficulty buttons.
+
+  */
+  function handleInputs() {
+    if (keyIsDown(ENTER)) {
+      console.log("Enter pressed... resetting game.");
+      resetGame();
+    } else if (keyIsDown(SHIFT)) {
+      console.log("Increasing difficulty.");
+      increaseDifficulty();
+    }
+  }
