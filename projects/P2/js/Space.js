0e0486e610b74e559f9068e11222ace7a9c68b6e
Sylvain Tran <45033804+peelingSun@users.noreply.github.com>
2019-10-28 14:12:03 -0400

    P2: Added displayTile() to test a grid display.

--- projects/P2/js/Space.js
+++ projects/P2/js/Space.js
@@ -9,4 +9,17 @@ class Space extends StaticElement {
     super(x, y);
     this.elementType = spaceTypeId.EMPTY;
   }
+
+  /**
+    Displays a tile border aroud this space element for style.
+
+  */
+  displayTile() {
+    push();
+    rectMode(CENTER);
+    noFill();
+    strokeWeight(50);
+    rect(this.x, this.y, 20, 20);
+    pop();
+  }
 }
--- projects/P2/js/script.js
+++ projects/P2/js/script.js
@@ -44,15 +44,12 @@ function setup() {
   const tileMapHeight = windowHeight;
 
   createEmptyTileMap(tileMapSize);
-  createWallElements(tileMapWidth, tileMapHeight);
 
   console.log(tileMapSize);
   console.log(tileMapHeight);
   console.log(tileMap.length);
-  tileMapExplorer = new TileMapExplorer(tileMap);
-  //tileMapExplorer.chartTileMap(tileMapSize);
 
-  //noLoop();
+  tileMapExplorer = new TileMapExplorer(tileMap);
 }
 
 /**
@@ -71,6 +68,7 @@ function createEmptyTileMap(tileMapSize) {
       // We add empty spaces first--TODO don't add where there is going to be walls
       let newSpace = new Space(k, m);
       tileMap[k][m] = newSpace;
+      tileMap[k][m].displayTile();
       //console.log("Coords X" + tileMap[k][m].spacePositionX);
       //console.log("Coords Y" + tileMap[k][m].spacePositionY);
       if(k === 639 && m === 639){
@@ -82,6 +80,7 @@ function createEmptyTileMap(tileMapSize) {
 
 /**
   Fills the tiles in the tileMap with Wall static elements to create borders.
+  TDDO to be implemented in an interactive way instead.
 
 */
 function createWallElements(tileMapWidth, tileMapHeight) {
@@ -89,46 +88,7 @@ function createWallElements(tileMapWidth, tileMapHeight) {
 
   for(let k = 0; k < tileMapWidth; k++) {
     for(let m = 0; m < tileMapHeight; m++) {
-      // if we are at the borders of the tileMap:
-      // xxxxxxxxxxxxxxxxx    <- 0
-      // x               x
-      // x               x
-      // xxxxxxxxxxxxxxxxx    <- tileMapSize
 
-      // replace that cell with a Wall Element
-      // if we are at k = 0, tileMapSize (top border)
-      // if we are at k = 0, and m to tileMapSize (left border)
-      // if we are at k to tileMapSize, m = tileMapSize (TODO cut the rest of the m) (lower border)
-      // if we are at k = tileMapSize, m to tileMapSize (right border)
-
-      if(m === 0) { // put a wall over the first row (top border)
-        newWallElement = new Wall(k, m);
-        tileMap[k][m] = newWallElement;
-        tileMap[k][m].drawWall();
-        console.log("Wall at X: " + tileMap[k][m].elementPositionX);
-        console.log("Wall at Y: " + tileMap[k][m].elementPositionY);
-      }
-      else if(k === 0) { // Left border
-        newWallElement = new Wall(k, m);
-        tileMap[k][m] = newWallElement;
-        tileMap[k][m].drawWall();
-        //console.log("Wall at X: " + tileMap[k][m].wallPositionX);
-        //console.log("Wall at Y: " + tileMap[k][m].wallPositionY);
-      }
-      else if(m === tileMapHeight - 1) { // Bottom border
-        newWallElement = new Wall(k, m);
-        tileMap[k][m] = newWallElement;
-        tileMap[k][m].drawWall();
-        //console.log("Wall at X: " + tileMap[k][m].wallPositionX);
-        //console.log("Wall at Y: " + tileMap[k][m].wallPositionY);
-      }
-      else if(k === tileMapWidth - 1) { // Right border
-        newWallElement = new Wall(k, m);
-        tileMap[k][m] = newWallElement;
-        tileMap[k][m].drawWall();
-        //console.log("Wall at X: " + tileMap[k][m].wallPositionX);
-        //console.log("Wall at Y: " + tileMap[k][m].wallPositionY);
-      }
     }
   }
 }
