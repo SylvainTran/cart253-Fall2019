0f781e1e18669e5ba9d96d60c5420e04a6592947
Sylvain Tran <45033804+peelingSun@users.noreply.github.com>
2019-10-31 14:16:57 -0400

    Ex6: Fix #5
    Problem: Typo in second argument of the constructor.
    Fixed: Replaced argument (why -> y).

--- exercises/exercise6/js/Prey.js
+++ exercises/exercise6/js/Prey.js
@@ -14,7 +14,8 @@ class Prey {
   //
   // Sets the initial values for the Predator's properties
   // Either sets default values or uses the arguments provided
-  constructor(x, why, speed, fillColor, radius) {
+//////////////// FIXED #2: Fixed typo in constructor argument (why -> y)
+  constructor(x, y, speed, fillColor, radius) {
     // Position
     this.x = x;
     this.y = y;
