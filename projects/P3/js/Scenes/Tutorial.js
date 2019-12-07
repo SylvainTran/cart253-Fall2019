ff8b8df89a5b61ea1520286d28a9b30fcfa50625
SylvainTran <sylvain.serey.tran@protonmail.com>
2019-12-07 13:32:54 -0500

    P3: Modified instructions for movement.
    Situation: Dated information.
    Changes: Modified instructions for movement.

--- projects/P3/js/Scenes/Tutorial.js
+++ projects/P3/js/Scenes/Tutorial.js
@@ -17,7 +17,8 @@ class Tutorial extends State {
     background(0);
     textSize(100);
     fill(255);
-    text("Welcome to Allison's Life Movie Theater.\nPress any mouse button to put on your VR glasses.\nIf you want to get up from your seat, use the key arrows.", -150, -150);
+    text("Welcome to Allison's Life Movie Theater.\nPress any mouse button to put on your VR glasses.", -150, -150);
+    text("If you want to get up from your seat, use the WASD keys.", -150, -600);
     pop();
     this.updateText();
   }
